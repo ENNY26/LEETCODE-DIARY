7036257a0972f8bb3ed0d24537bda30126bcaba5
eniola orehin <eniola.orehin@cix.csi.cuny.edu>
2025-01-15 23:36:12 -0500

    implement hasCycle function to detect a cycle in a linked list

---
+++ lc-141.js
@@ -0,0 +1,15 @@
+var hasCycle = function(head) {
+    if(!head || !head.next) return false
+
+    let fast =  head
+    let slow = head
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+
+        if(slow === fast ){
+            return true;
+        }
+    } return false
+
+};
