6de1f4e755aa1d54cda1b2943e69dae418a51e90
eniola orehin <eniola.orehin@cix.csi.cuny.edu>
2025-02-16 23:48:49 -0500

    implement findMin function to find the minimum element in a rotated sorted array

---
+++ lc-153.js
@@ -0,0 +1,12 @@
+var findMin = function(nums) {
+    let low = 0, high  = nums.length - 1
+
+   while (low < high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] > nums[high]) {
+        low = mid + 1;  // Search right
+    } else {
+        high = mid;  // Search left
+    }
+}
