0494053ffff95f82b3d9544c68463fd04d83deaa
eniola orehin <eniola.orehin@cix.csi.cuny.edu>
2025-04-21 23:53:42 -0400

    implement averageLevelSum function to calculate the average value of nodes at each level in a binary tree

---
+++ lc-637.js
@@ -0,0 +1,17 @@
+while(queue.length > 0){
+    let levelSum = 0
+    let size = queue.length
+
+    for(let i = 0; i<size; i++){
+        let node = queue.shift()
+        levelSum+=node.val
+
+     if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    result.push(levelSum / size);
+}
+
+return result;
+};
