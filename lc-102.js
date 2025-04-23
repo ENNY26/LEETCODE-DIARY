bb938f7bf1bdd7f30480da178c44392ec51d4f72
eniola orehin <eniola.orehin@cix.csi.cuny.edu>
2025-04-22 23:41:31 -0400

    implement levelOrder function for performing level order traversal of a binary tree

---
+++ lc-102.js
@@ -0,0 +1,23 @@
+var levelOrder = function (root) {
+    if (!root) return []
+        let queue = [root]
+        let result = []
+    
+        while (queue.length > 0) {
+            let level =[]
+            let size = queue.length
+    
+            for (let i = 0; i < size; i++) {
+                let node = queue.shift()
+                level.push(node.val)
+    
+                if (node.left) queue.push(node.left);
+                if (node.right) queue.push(node.right);
+    
+            }
+    
+        
+         result.push(level)
+        }
+        return result
+    };
